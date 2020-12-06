aa62344f3e681557fb90f42d569cb448fdb1d8fe
Faizaan Gagan <fzngagan@gmail.com>
2020-12-07 00:53:00 +0530

    Fixed server code formatting with rubocop

--- plugin.rb
+++ plugin.rb
@@ -8,21 +8,20 @@
 
 enabled_site_setting :tlp_thumbnail_enabled
 
-
 after_initialize do
-    register_topic_custom_field_type('external_thumbnail_url', :string)
+  register_topic_custom_field_type('external_thumbnail_url', :string)
     add_preloaded_topic_list_custom_field('external_thumbnail_url')
-    
+
     PostRevisor.track_topic_field(:external_thumbnail_url) do |tc, attribute|
-        tc.record_change('external_thumbnail_url', tc.topic.custom_fields['external_thumbnail_url'], attribute)
+      tc.record_change('external_thumbnail_url', tc.topic.custom_fields['external_thumbnail_url'], attribute)
         tc.topic.custom_fields['external_thumbnail_url'] = attribute
     end
 
     add_to_serializer(:topic_view, :external_thumbnail_url) do
-        object.topic.custom_fields['external_thumbnail_url']
+      object.topic.custom_fields['external_thumbnail_url']
     end
 
     add_to_serializer(:topic_list_item, :external_thumbnail_url) do
-        object.custom_fields['external_thumbnail_url']
+      object.custom_fields['external_thumbnail_url']
     end
 end
